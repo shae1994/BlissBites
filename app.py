cca1353f86013edd3b929d4d5a21afdc9c4b69ac
Juddy-Shae Wallace <juddy-shae@Juddy-Shaes-MacBook-Pro.local>
2023-08-11 08:19:27 -0500

    complete

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
 from flask import Flask, render_template, redirect, url_for
-
+from data import products as product_list
 
 app = Flask(__name__)
-
-
-@app.get("/products")
-def products():
-    return 'This is the products route'
 
 
 @app.route('/')
 def home():  # put application's code here
     return render_template('index.html')
+
+
+@app.route('/products')
+def products():
+    return render_template('products.html', products=product_list)
+
+
+from flask import Flask, render_template, redirect, url_for
+from data import products as product_list
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/products')
+def products():
+    return render_template('products.html', products=product_list)
+
+
+@app.route('/products/<int:product_id>')
+def product_detail(product_id):
+    product = next((item for item in product_list if item['id'] == product_id), None)
+    if not product:
+        return "Product not found", 404
+    return render_template('product_detail.html', product=product)
 
 
 if __name__ == '__main__':
---
+++ data.py
@@ -0,0 +1,58 @@
+products = [
+    {
+        'id': 1,
+        'name':'Berrylicious Fusion',
+        'image_location': 'images/products/berrylicious.png',
+        'product-image':'',
+        'cost': 2.50,
+        'description': "Indulge in the delightful harmony of our Berrylicious Fusion cupcake. Crafted with plump "
+                       "strawberries and wild blueberries, this masterpiece marries the sweetness of sun-ripened "
+                       "strawberries with the vibrant tang of blueberries. Each bite is a burst of nature's bounty, "
+                       "with real fruit essence creating a symphony of flavors that'll dance on your taste buds."
+    },
+    {
+        'id': 2,
+        'name':'Vanilla Skies Delight',
+        'image_location': 'images/products/vanilla-skies.png',
+        'product-image':'',
+        'cost': 3,
+        'description': "Take a whimsical journey with our Vanilla Skies Delight cupcake. Layers of velvety vanilla "
+                       "cake are like clouds in a bright blue sky. Topped with rainbow sprinkles that twinkle like "
+                       "stars, this cupcake is a joyful blend of vanilla's comforting embrace and the playful magic "
+                       "of colorful confetti. Every bite is a dreamy escape into a world of sweet vanilla skies."
+    },
+    {
+        'id': 3,
+        'name': 'CocoBerry Bliss ',
+        'image_location': 'images/products/cocoberry-blis.png',
+        'product-image':'',
+        'cost': 5,
+        'description': "Experience sheer bliss with our CocoBerry Bliss cupcake. A fusion of luscious strawberries "
+                       "and rich cocoa, this creation is a testimony to taste perfection. Fresh strawberries bring a "
+                       "burst of natural sweetness, while cocoa infuses depth and decadence. Topped with a swirl of "
+                       "creamy chocolate frosting, it's a delightful journey into the heart of cocoa-coated berry "
+                       "ecstasy."
+    },{
+        'id': 4,
+        'name': 'BLueberry Blast',
+        'image_location': 'images/products/bb-blast.png',
+        'product-image':'',
+        'cost': 4,
+        'description': "Savor the enchantment of our BlueBerry Bliss cupcake. Packed with plump blueberries that "
+                       "burst with flavor in every bite, this treat captures the essence of a sunny blueberry patch. "
+                       "Baked into a moist and fluffy cake, these blueberries create a symphony of sweet-tart bliss. "
+                       "Topped with a delicate blueberry drizzle, it's a delectable ode to nature's berry treasure."
+    },{
+        'id': 5,
+        'name': 'Lemon Poppy',
+        'image_location': 'images/products/coming-soon.png',
+        'cost': 4,
+        'description': 'Product 2 description here.'
+    },{
+        'id': 6,
+        'name': 'Blueberry Cheesecake',
+        'image_location': 'images/products/coming-soon.png',
+        'cost': 4,
+        'description': 'Product 2 description here.'
+    },
+]
