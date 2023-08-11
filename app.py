66de5e041c0d4458af8a37a91a089e72f3488af1
Juddy-Shae Wallace <juddy-shae@Juddy-Shaes-MacBook-Pro.local>
2023-08-10 21:51:51 -0500

    Added Static Files

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+
 
 app = Flask(__name__)
 
 
+@app.get("/products")
+def products():
+    return 'This is the products route'
+
+
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
